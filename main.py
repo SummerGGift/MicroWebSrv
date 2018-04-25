e1e7be0d8af04221b17e1e52e3517f5e3a5063af
SummerGift <459994202@qq.com>
2018-04-25 18:34:12 +0800

    【修改】：添加 返回 json 对象功能

--- main.py
+++ main.py
@@ -1,45 +1,47 @@
 
 from microWebSrv import MicroWebSrv
+import rtthread
+import json
 
 # ----------------------------------------------------------------------------
 
 @MicroWebSrv.route('/test')
 def _httpHandlerTestGet(httpClient, httpResponse) :
-	content = """\
-	<!DOCTYPE html>
-	<html lang=en>
+    content = """\
+    <!DOCTYPE html>
+    <html lang=en>
         <head>
-        	<meta charset="UTF-8" />
+            <meta charset="UTF-8" />
             <title>TEST GET</title>
         </head>
         <body>
             <h1>TEST GET</h1>
             Client IP address = %s
             <br />
-			<form action="/test" method="post" accept-charset="ISO-8859-1">
-				First name: <input type="text" name="firstname"><br />
-				Last name: <input type="text" name="lastname"><br />
-				<input type="submit" value="Submit">
-			</form>
+            <form action="/test" method="post" accept-charset="ISO-8859-1">
+                First name: <input type="text" name="firstname"><br />
+                Last name: <input type="text" name="lastname"><br />
+                <input type="submit" value="Submit">
+            </form>
         </body>
     </html>
-	""" % httpClient.GetIPAddr()
-	httpResponse.WriteResponseOk( headers		 = None,
-								  contentType	 = "text/html",
-								  contentCharset = "UTF-8",
-								  content 		 = content )
+    """ % httpClient.GetIPAddr()
+    httpResponse.WriteResponseOk( headers        = None,
+                                  contentType    = "text/html",
+                                  contentCharset = "UTF-8",
+                                  content        = content )
 
 
 @MicroWebSrv.route('/test', 'POST')
 def _httpHandlerTestPost(httpClient, httpResponse) :
-	formData  = httpClient.ReadRequestPostedFormData()
-	firstname = formData["firstname"]
-	lastname  = formData["lastname"]
-	content   = """\
-	<!DOCTYPE html>
-	<html lang=en>
-		<head>
-			<meta charset="UTF-8" />
+    formData  = httpClient.ReadRequestPostedFormData()
+    firstname = formData["firstname"]
+    lastname  = formData["lastname"]
+    content   = """\
+    <!DOCTYPE html>
+    <html lang=en>
+        <head>
+            <meta charset="UTF-8" />
             <title>TEST POST</title>
         </head>
         <body>
@@ -48,73 +50,88 @@ def _httpHandlerTestPost(httpClient, httpResponse) :
             Lastname = %s<br />
         </body>
     </html>
-	""" % ( MicroWebSrv.HTMLEscape(firstname),
-		    MicroWebSrv.HTMLEscape(lastname) )
-	httpResponse.WriteResponseOk( headers		 = None,
-								  contentType	 = "text/html",
-								  contentCharset = "UTF-8",
-								  content 		 = content )
+    """ % ( MicroWebSrv.HTMLEscape(firstname),
+            MicroWebSrv.HTMLEscape(lastname) )
+    httpResponse.WriteResponseOk( headers        = None,
+                                  contentType    = "text/html",
+                                  contentCharset = "UTF-8",
+                                  content        = content )
 
+@MicroWebSrv.route('/sysdata')
+def _httpHandlerTestGet(httpClient, httpResponse) :
+    content ={
+        "cpu_usage" : "52%",
+        "mem_usage" : (29877028,10000,100000),
+        "ip_addr"   : '192.168.10.110',
+        'led1'      : True,
+        'key'       : True,
+        'sensor'    : 28.5}
+    json_content = json.dumps(content)
+
+    httpResponse.WriteResponseOk( headers        = None,
+                                  contentType    = "text/json",
+                                  contentCharset = "UTF-8",
+                                  content        = json_content)
 
 @MicroWebSrv.route('/edit/<index>')             # <IP>/edit/123           ->   args['index']=123
 @MicroWebSrv.route('/edit/<index>/abc/<foo>')   # <IP>/edit/123/abc/bar   ->   args['index']=123  args['foo']='bar'
 @MicroWebSrv.route('/edit')                     # <IP>/edit               ->   args={}
 def _httpHandlerEditWithArgs(httpClient, httpResponse, args={}) :
-	content = """\
-	<!DOCTYPE html>
-	<html lang=en>
+    content = """\
+    <!DOCTYPE html>
+    <html lang=en>
         <head>
-        	<meta charset="UTF-8" />
+            <meta charset="UTF-8" />
             <title>TEST EDIT</title>
         </head>
         <body>
-	"""
-	content += "<h1>EDIT item with {} variable arguments</h1>"\
-		.format(len(args))
-	
-	if 'index' in args :
-		content += "<p>index = {}</p>".format(args['index'])
-	
-	if 'foo' in args :
-		content += "<p>foo = {}</p>".format(args['foo'])
-	
-	content += """
+    """
+    content += "<h1>EDIT item with {} variable arguments</h1>"\
+        .format(len(args))
+    
+    if 'index' in args :
+        content += "<p>index = {}</p>".format(args['index'])
+    
+    if 'foo' in args :
+        content += "<p>foo = {}</p>".format(args['foo'])
+    
+    content += """
         </body>
     </html>
-	"""
-	httpResponse.WriteResponseOk( headers		 = None,
-								  contentType	 = "text/html",
-								  contentCharset = "UTF-8",
-								  content 		 = content )
+    """
+    httpResponse.WriteResponseOk( headers        = None,
+                                  contentType    = "text/html",
+                                  contentCharset = "UTF-8",
+                                  content        = content )
 
 # ----------------------------------------------------------------------------
 
 def _acceptWebSocketCallback(webSocket, httpClient) :
-	print("WS ACCEPT")
-	webSocket.RecvTextCallback   = _recvTextCallback
-	webSocket.RecvBinaryCallback = _recvBinaryCallback
-	webSocket.ClosedCallback 	 = _closedCallback
+    print("WS ACCEPT")
+    webSocket.RecvTextCallback   = _recvTextCallback
+    webSocket.RecvBinaryCallback = _recvBinaryCallback
+    webSocket.ClosedCallback     = _closedCallback
 
 def _recvTextCallback(webSocket, msg) :
-	print("WS RECV TEXT : %s" % msg)
-	webSocket.SendText("Reply for %s" % msg)
+    print("WS RECV TEXT : %s" % msg)
+    webSocket.SendText("Reply for %s" % msg)
 
 def _recvBinaryCallback(webSocket, data) :
-	print("WS RECV DATA : %s" % data)
+    print("WS RECV DATA : %s" % data)
 
 def _closedCallback(webSocket) :
-	print("WS CLOSED")
+    print("WS CLOSED")
 
 # ----------------------------------------------------------------------------
 
 #routeHandlers = [
-#	( "/test",	"GET",	_httpHandlerTestGet ),
-#	( "/test",	"POST",	_httpHandlerTestPost )
+#   ( "/test",  "GET",  _httpHandlerTestGet ),
+#   ( "/test",  "POST", _httpHandlerTestPost )
 #]
 
 srv = MicroWebSrv(webPath='www/')
 srv.MaxWebSocketRecvLen     = 256
-srv.WebSocketThreaded		= False
+srv.WebSocketThreaded       = False
 srv.AcceptWebSocketCallback = _acceptWebSocketCallback
 srv.Start(threaded=False)
 
